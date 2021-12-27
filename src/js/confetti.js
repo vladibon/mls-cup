3029ea4db973770f061b116d55ac00d7be99f1df
Vladimir Bondar <vladi.bondar@gmail.com>
2021-12-27 23:05:45 +0200

    delete extra plugins

--- src/js/confetti.js
+++ src/js/confetti.js
@@ -1,51 +1,3 @@
-// const confetti = require('canvas-confetti');
-
-// var end = Date.now() + 6 * 1000;
-// var colors = ['#a66f01', '#777'];
-
-// (function frame() {
-//   confetti.default({
-//     particleCount: 2,
-//     angle: 90,
-//     spread: 160,
-//     startVelocity: 110,
-//     gravity: 0.9,
-//     ticks: 700,
-//     origin: { y: 1.2 },
-//     colors: colors,
-//     shapes: 'square',
-//     scalar: 2,
-//   });
-//   confetti.default({
-//     particleCount: 2,
-//     angle: 90,
-//     spread: 160,
-//     startVelocity: 110,
-//     gravity: 0.9,
-//     ticks: 700,
-//     origin: { y: 1.2 },
-//     colors: colors,
-//     shapes: 'square',
-//     scalar: 1,
-//   });
-//   confetti.default({
-//     particleCount: 2,
-//     angle: 90,
-//     spread: 160,
-//     startVelocity: 110,
-//     gravity: 0.9,
-//     ticks: 700,
-//     origin: { y: 1.2 },
-//     colors: colors,
-//     shapes: 'square',
-//     scalar: 0.5,
-//   });
-
-//   if (Date.now() < end) {
-//     requestAnimationFrame(frame);
-//   }
-// })();
-
 import { getConfettiRefs } from './refs';
 
 function runConfetti() {
@@ -62,8 +14,8 @@ function runConfetti() {
       confettiFragment.appendChild(confettiGroup.cloneNode(true));
     }
 
-    refs.confettiContainerRef.appendChild(confettiFragment);
-  }, 700);
+    refs.confettiContainerRef.prepend(confettiFragment);
+  }, 500);
 
   setTimeout(() => clearInterval(intervalId), 5000);
 }
